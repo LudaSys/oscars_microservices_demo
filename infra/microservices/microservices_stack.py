6c8e7824b8580630fd1e5905f8ede4407d37a802
John Slowik <john@jslowik.com>
2022-12-29 16:15:55 -0600

    Formatting

--- infra/microservices/microservices_stack.py
+++ infra/microservices/microservices_stack.py
@@ -20,32 +20,32 @@ class OscarsMicroservicesStack(Stack):
         user_choices_table = ddb.Table(
             self,
             "choices_table",
-            table_name="user_choices",
-            read_capacity=1,
-            write_capacity=1,
-            billing_mode=ddb.BillingMode.PROVISIONED,
-            removal_policy=aws_cdk.RemovalPolicy.DESTROY,
-            partition_key=ddb.Attribute(name="year", type=ddb.AttributeType.NUMBER),
-            sort_key=ddb.Attribute(name="user_id", type=ddb.AttributeType.STRING)
+            table_name = "user_choices",
+            read_capacity = 1,
+            write_capacity = 1,
+            billing_mode = ddb.BillingMode.PROVISIONED,
+            removal_policy = aws_cdk.RemovalPolicy.DESTROY,
+            partition_key = ddb.Attribute(name = "year", type = ddb.AttributeType.NUMBER),
+            sort_key = ddb.Attribute(name = "user_id", type = ddb.AttributeType.STRING)
         )
 
         # Auto scaling rules
         user_choices_table.auto_scale_write_capacity(
-            max_capacity=5,
-            min_capacity=1
+            max_capacity = 5,
+            min_capacity = 1
         )
 
         user_choices_table.auto_scale_read_capacity(
-            max_capacity=5,
-            min_capacity=1
-        ).scale_on_utilization(target_utilization_percent=80)
+            max_capacity = 5,
+            min_capacity = 1
+        ).scale_on_utilization(target_utilization_percent = 80)
 
         # Role for lambdas
         lambda_role = iam.Role(
             self,
             "microservice_lambda_role",
-            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
-            managed_policies=[
+            assumed_by = iam.ServicePrincipal("lambda.amazonaws.com"),
+            managed_policies = [
                 iam.ManagedPolicy.from_aws_managed_policy_name("CloudWatchFullAccess"),
             ],
         )
@@ -55,14 +55,14 @@ class OscarsMicroservicesStack(Stack):
             iam.Policy(
                 self,
                 "oscars_lambda_db_access_inline_policy",
-                policy_name="oscars_db_access_policy",
-                statements=[
+                policy_name = "oscars_db_access_policy",
+                statements = [
                     iam.PolicyStatement(
-                        effect=iam.Effect.ALLOW,
-                        actions=[
+                        effect = iam.Effect.ALLOW,
+                        actions = [
                             'dynamodb:*'
                         ],
-                        resources=[user_choices_table.table_arn]
+                        resources = [user_choices_table.table_arn]
                     )
                 ]
             )
@@ -72,34 +72,34 @@ class OscarsMicroservicesStack(Stack):
         read_item_lambda = lam.Function(
             self,
             "read_value",
-            runtime=lam.Runtime.PYTHON_3_9,
-            memory_size=128,
-            timeout=aws_cdk.Duration.seconds(60),
-            code=lam.AssetCode.from_asset("../functions/read_items"),
-            handler="read_items.handler",
-            architecture=lam.Architecture.ARM_64,
-            role=lambda_role,
-            log_retention=logs.RetentionDays.ONE_WEEK,
-            current_version_options=lam.VersionOptions(removal_policy=aws_cdk.RemovalPolicy.DESTROY),
-            environment={
+            runtime = lam.Runtime.PYTHON_3_9,
+            memory_size = 128,
+            timeout = aws_cdk.Duration.seconds(60),
+            code = lam.AssetCode.from_asset("../functions/read_items"),
+            handler = "read_items.handler",
+            architecture = lam.Architecture.ARM_64,
+            role = lambda_role,
+            log_retention = logs.RetentionDays.ONE_WEEK,
+            current_version_options = lam.VersionOptions(removal_policy = aws_cdk.RemovalPolicy.DESTROY),
+            environment = {
                 "DYNAMODB_TABLE": "user_choices"
             }
         )
-        
+
         # Scan items lambda
         scan_item_lambda = lam.Function(
             self,
             "scan_items_oscars",
             runtime = lam.Runtime.PYTHON_3_9,
-            memory_size=128,
-            timeout=aws_cdk.Duration.seconds(60),
-            code=lam.AssetCode.from_asset("../functions/scan_items"),
-            handler="scan_items.handler",
-            architecture=lam.Architecture.ARM_64,
-            role=lambda_role,
-            log_retention=logs.RetentionDays.ONE_WEEK,
-            current_version_options=lam.VersionOptions(removal_policy=aws_cdk.RemovalPolicy.DESTROY),
-            environment={
+            memory_size = 128,
+            timeout = aws_cdk.Duration.seconds(60),
+            code = lam.AssetCode.from_asset("../functions/scan_items"),
+            handler = "scan_items.handler",
+            architecture = lam.Architecture.ARM_64,
+            role = lambda_role,
+            log_retention = logs.RetentionDays.ONE_WEEK,
+            current_version_options = lam.VersionOptions(removal_policy = aws_cdk.RemovalPolicy.DESTROY),
+            environment = {
                 "DYNAMODB_TABLE": "user_choices"
             }
         )
@@ -108,53 +108,53 @@ class OscarsMicroservicesStack(Stack):
         rest_api = gateway.RestApi(
             self,
             "oscars-api",
-            rest_api_name="OscarsLambdaApi",
-            retain_deployments=False,
-            endpoint_export_name="OscarsRestApiUrl",
-            deploy=True,
-            endpoint_types=[gateway.EndpointType.REGIONAL],
-            deploy_options=gateway.StageOptions(
-                stage_name="dev",
-                caching_enabled=False,
-                cache_ttl=aws_cdk.Duration.minutes(1),
-                throttling_burst_limit=100,
-                throttling_rate_limit=1000
+            rest_api_name = "OscarsLambdaApi",
+            retain_deployments = False,
+            endpoint_export_name = "OscarsRestApiUrl",
+            deploy = True,
+            endpoint_types = [gateway.EndpointType.REGIONAL],
+            deploy_options = gateway.StageOptions(
+                stage_name = "dev",
+                caching_enabled = False,
+                cache_ttl = aws_cdk.Duration.minutes(1),
+                throttling_burst_limit = 100,
+                throttling_rate_limit = 1000
             )
         )
 
         # Scan resource and endpoint
         scan_endpoint = rest_api.root.add_resource("scan")
         scan_endpoint.add_method(
-            http_method="GET",
-            integration=gateway.LambdaIntegration(scan_item_lambda),
-            api_key_required=True
+            http_method = "GET",
+            integration = gateway.LambdaIntegration(scan_item_lambda),
+            api_key_required = True
         )
 
         # Read resource and endpoint
         read_endpoint = rest_api.root.add_resource("read")
         read_endpoint.add_method(
-            http_method="GET",
-            integration=gateway.LambdaIntegration(read_item_lambda),
-            api_key_required=True
+            http_method = "GET",
+            integration = gateway.LambdaIntegration(read_item_lambda),
+            api_key_required = True
         )
 
         # Usage plan
         usage_plan = rest_api.add_usage_plan(
             "OscarsApiUsagePlan",
-            name="OscarsUsagePlan",
-            throttle=gateway.ThrottleSettings(
-                burst_limit=10,
-                rate_limit=100
+            name = "OscarsUsagePlan",
+            throttle = gateway.ThrottleSettings(
+                burst_limit = 10,
+                rate_limit = 100
             ),
-            quota=gateway.QuotaSettings(
-                limit=100,
-                offset=0,
-                period=gateway.Period.DAY
+            quota = gateway.QuotaSettings(
+                limit = 100,
+                offset = 0,
+                period = gateway.Period.DAY
             ),
-            api_stages=[
+            api_stages = [
                 gateway.UsagePlanPerApiStage(
-                    api=rest_api,
-                    stage=rest_api.deployment_stage
+                    api = rest_api,
+                    stage = rest_api.deployment_stage
                 )
             ]
         )
@@ -162,8 +162,8 @@ class OscarsMicroservicesStack(Stack):
         # API key
         api_key = rest_api.add_api_key(
             "OscarsApiKey",
-            api_key_name="oscars_api_key",
-            description="API key for Oscars Gateway"
+            api_key_name = "oscars_api_key",
+            description = "API key for Oscars Gateway"
         )
 
         # Attach api key to use plan
